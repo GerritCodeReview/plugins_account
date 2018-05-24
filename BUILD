GerritForge/account

Initial version of the account removal plugin
The account plugin for Gerrit is designed to allow companies to
improve compliance with the GDPR requirements:

- Ability for an individual to display the personal information that
  Gerrit holds about him
- Ability to "self-remove" the personal information from Gerrit

NOTE: This plugin itself is not giving any GDPR certification or
      compliance. You would need to read carefully the EU law and
      apply to your Company context and organisation.

BUILD
@@ -1,0 +1,35 @@
+load("//tools/bzl:junit.bzl", "junit_tests")
+load(
+    "//tools/bzl:plugin.bzl",
+    "gerrit_plugin",
+    "PLUGIN_DEPS",
+    "PLUGIN_TEST_DEPS",
+)
+
+gerrit_plugin(
+    name = "account",
+    srcs = glob(["src/main/java/**/*.java"]),
+    manifest_entries = [
+        "Gerrit-PluginName: account",
+        "Gerrit-Module: com.googlesource.gerrit.plugins.account.Module",
+        "Gerrit-SshModule: com.googlesource.gerrit.plugins.account.SshModule",
+    ],
+    resources = glob(["src/main/resources/**/*"]),
+)
+
+junit_tests(
+    name = "account_tests",
+    srcs = glob(["src/test/java/**/*.java"]),
+    tags = ["account"],
+    deps = [":account__plugin_test_deps"],
+)
+
+java_library(
+    name = "account__plugin_test_deps",
+    testonly = 1,
+    visibility = ["//visibility:public"],
+    exports = PLUGIN_DEPS + PLUGIN_TEST_DEPS + [
+        ":account__plugin",
+        "@mockito//jar",
+    ],
+)

src/main/java/com/googlesource/gerrit/plugins/account/AccountPersonalInformation.java
@@ -1,0 +1,30 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account;
+
+import com.google.gerrit.server.IdentifiedUser;
+import java.util.Set;
+
+public class AccountPersonalInformation {
+  public final String fullname;
+  public final String username;
+  public final Set<String> emails;
+
+  public AccountPersonalInformation(IdentifiedUser user) {
+    this.fullname = user.getName();
+    this.username = user.getUserName();
+    this.emails = user.getEmailAddresses();
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/account/AccountRemover.java
@@ -1,0 +1,25 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account;
+
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(GerritAccountRemover.class)
+public interface AccountRemover {
+
+  void removeAccount(int accountId) throws Exception;
+
+  boolean canDelete(int accountId);
+}

src/main/java/com/googlesource/gerrit/plugins/account/DeleteAccountResponse.java
@@ -1,0 +1,25 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account;
+
+public class DeleteAccountResponse {
+  public final boolean deleted;
+  public final AccountPersonalInformation accountInfo;
+
+  public DeleteAccountResponse(boolean deleted, AccountPersonalInformation accountInfo) {
+    this.deleted = deleted;
+    this.accountInfo = accountInfo;
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/account/Module.java
@@ -1,0 +1,45 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account;
+
+import static com.google.gerrit.server.account.AccountResource.ACCOUNT_KIND;
+
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.extensions.config.CapabilityDefinition;
+import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.inject.AbstractModule;
+import com.googlesource.gerrit.plugins.account.permissions.DeleteAccountCapability;
+import com.googlesource.gerrit.plugins.account.permissions.DeleteOwnAccountCapability;
+
+class Module extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    install(
+        new RestApiModule() {
+          @Override
+          protected void configure() {
+            delete(ACCOUNT_KIND).to(DeleteAccount.class);
+          }
+        });
+
+    bind(CapabilityDefinition.class)
+        .annotatedWith(Exports.named(DeleteAccountCapability.DELETE_ACCOUNT))
+        .to(DeleteAccountCapability.class);
+    bind(CapabilityDefinition.class)
+        .annotatedWith(Exports.named(DeleteOwnAccountCapability.DELETE_OWN_ACCOUNT))
+        .to(DeleteOwnAccountCapability.class);
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/account/SshModule.java
@@ -1,0 +1,24 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account;
+
+import com.google.gerrit.sshd.PluginCommandModule;
+
+public class SshModule extends PluginCommandModule {
+  @Override
+  protected void configureCommands() {
+    command(DeleteAccountCommand.class);
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/account/permissions/DeleteAccountCapability.java
@@ -1,0 +1,26 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account.permissions;
+
+import com.google.gerrit.extensions.config.CapabilityDefinition;
+
+public class DeleteAccountCapability extends CapabilityDefinition {
+  public static final String DELETE_ACCOUNT = "deleteAccount";
+
+  @Override
+  public String getDescription() {
+    return "Delete any account";
+  }
+}

src/test/java/com/googlesource/gerrit/plugins/account/test/DeleteAccountTest.java
@@ -1,0 +1,91 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account.test;
+
+import static org.mockito.Mockito.*;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.googlesource.gerrit.plugins.account.AccountRemover;
+import com.googlesource.gerrit.plugins.account.DeleteAccount;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteAccountTest {
+
+  @Mock private AccountRemover accountRemoverMock;
+
+  @Mock private AccountResource accountResourceMock;
+
+  @Mock private IdentifiedUser userMock;
+
+  @Before
+  public void setup() {
+    when(accountResourceMock.getUser()).thenReturn(userMock);
+  }
+
+  @Test
+  public void givenAccountWithPermissions_whenRunningDeleteAccount_thenAccountRemoverIsInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock).removeAccount(accountId);
+  }
+
+  @Test(expected = AuthException.class)
+  public void givenAccountWithoutPermissions_whenRunningDeleteAccount_thenThrowAuthException()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(false);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  @Test
+  public void givenAccountWithNonMatching_whenRunningDeleteAccount_thenAccountRemoverIsNotInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "NonMatching Name";
+    mockUserData(accountId, "First Last");
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  private void mockUserData(int accountId, String accountName) {
+    when(userMock.getAccountId()).thenReturn(new Account.Id(accountId));
+    when(userMock.getName()).thenReturn(accountName);
+  }
+}

GerritCodeReview/plugins_account

Change-Id: I64cdaee2803b63e05285b054cecde7be6b390149

BUILD
@@ -1,0 +1,35 @@
+load("//tools/bzl:junit.bzl", "junit_tests")
+load(
+    "//tools/bzl:plugin.bzl",
+    "gerrit_plugin",
+    "PLUGIN_DEPS",
+    "PLUGIN_TEST_DEPS",
+)
+
+gerrit_plugin(
+    name = "account",
+    srcs = glob(["src/main/java/**/*.java"]),
+    manifest_entries = [
+        "Gerrit-PluginName: account",
+        "Gerrit-Module: com.gerritforge.gerrit.plugins.account.Module",
+        "Gerrit-SshModule: com.gerritforge.gerrit.plugins.account.SshModule",
+    ],
+    resources = glob(["src/main/resources/**/*"]),
+)
+
+junit_tests(
+    name = "account_tests",
+    srcs = glob(["src/test/java/**/*.java"]),
+    tags = ["account"],
+    deps = [":account__plugin_test_deps"],
+)
+
+java_library(
+    name = "account__plugin_test_deps",
+    testonly = 1,
+    visibility = ["//visibility:public"],
+    exports = PLUGIN_DEPS + PLUGIN_TEST_DEPS + [
+        ":account__plugin",
+        "@mockito//jar",
+    ],
+)

src/main/java/com/gerritforge/gerrit/plugins/account/AccountPersonalInformation.java
@@ -1,0 +1,30 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account;
+
+import com.google.gerrit.server.IdentifiedUser;
+import java.util.Set;
+
+public class AccountPersonalInformation {
+  public final String fullname;
+  public final String username;
+  public final Set<String> emails;
+
+  public AccountPersonalInformation(IdentifiedUser user) {
+    this.fullname = user.getName();
+    this.username = user.getUserName();
+    this.emails = user.getEmailAddresses();
+  }
+}

src/main/java/com/gerritforge/gerrit/plugins/account/AccountRemover.java
@@ -1,0 +1,25 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account;
+
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(GerritAccountRemover.class)
+public interface AccountRemover {
+
+  void removeAccount(int accountId) throws Exception;
+
+  boolean canDelete(int accountId);
+}

src/main/java/com/gerritforge/gerrit/plugins/account/DeleteAccountResponse.java
@@ -1,0 +1,25 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account;
+
+public class DeleteAccountResponse {
+  public final boolean deleted;
+  public final AccountPersonalInformation accountInfo;
+
+  public DeleteAccountResponse(boolean deleted, AccountPersonalInformation accountInfo) {
+    this.deleted = deleted;
+    this.accountInfo = accountInfo;
+  }
+}

src/main/java/com/gerritforge/gerrit/plugins/account/Module.java
@@ -1,0 +1,45 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account;
+
+import static com.google.gerrit.server.account.AccountResource.ACCOUNT_KIND;
+
+import com.gerritforge.gerrit.plugins.account.permissions.DeleteAccountCapability;
+import com.gerritforge.gerrit.plugins.account.permissions.DeleteOwnAccountCapability;
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.extensions.config.CapabilityDefinition;
+import com.google.gerrit.extensions.restapi.RestApiModule;
+import com.google.inject.AbstractModule;
+
+class Module extends AbstractModule {
+
+  @Override
+  protected void configure() {
+    install(
+        new RestApiModule() {
+          @Override
+          protected void configure() {
+            delete(ACCOUNT_KIND).to(DeleteAccount.class);
+          }
+        });
+
+    bind(CapabilityDefinition.class)
+        .annotatedWith(Exports.named(DeleteAccountCapability.DELETE_ACCOUNT))
+        .to(DeleteAccountCapability.class);
+    bind(CapabilityDefinition.class)
+        .annotatedWith(Exports.named(DeleteOwnAccountCapability.DELETE_OWN_ACCOUNT))
+        .to(DeleteOwnAccountCapability.class);
+  }
+}

src/main/java/com/gerritforge/gerrit/plugins/account/SshModule.java
@@ -1,0 +1,24 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account;
+
+import com.google.gerrit.sshd.PluginCommandModule;
+
+public class SshModule extends PluginCommandModule {
+  @Override
+  protected void configureCommands() {
+    command(DeleteAccountCommand.class);
+  }
+}

src/main/java/com/gerritforge/gerrit/plugins/account/permissions/DeleteAccountCapability.java
@@ -1,0 +1,26 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account.permissions;
+
+import com.google.gerrit.extensions.config.CapabilityDefinition;
+
+public class DeleteAccountCapability extends CapabilityDefinition {
+  public static final String DELETE_ACCOUNT = "deleteAccount";
+
+  @Override
+  public String getDescription() {
+    return "Delete any account";
+  }
+}

src/test/java/com/gerritforge/gerrit/plugins/account/test/DeleteAccountTest.java
@@ -1,0 +1,92 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account.test;
+
+import static org.mockito.Mockito.*;
+
+import com.gerritforge.gerrit.plugins.account.AccountRemover;
+import com.gerritforge.gerrit.plugins.account.DeleteAccount;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteAccountTest {
+
+  @Mock private AccountRemover accountRemoverMock;
+
+  @Mock private AccountResource accountResourceMock;
+
+  @Mock private IdentifiedUser userMock;
+
+  @Before
+  public void setup() {
+    when(accountResourceMock.getUser()).thenReturn(userMock);
+  }
+
+  @Test
+  public void givenAccountWithPermissions_whenRunningDeleteAccount_thenAccountRemoverIsInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock).removeAccount(accountId);
+  }
+
+  @Test(expected = AuthException.class)
+  public void givenAccountWithoutPermissions_whenRunningDeleteAccount_thenThrowAuthException()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(false);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  @Test
+  public void givenAccountWithNonMatching_whenRunningDeleteAccount_thenAccountRemoverIsNotInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "NonMatching Name";
+    mockUserData(accountId, "First Last");
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  private void mockUserData(int accountId, String accountName) {
+    when(userMock.getAccountId()).thenReturn(new Account.Id(accountId));
+    when(userMock.getName()).thenReturn(accountName);
+  }
+}