GerritForge/account

Initial version of the account removal plugin
The account plugin for Gerrit is designed to allow companies to
improve compliance with the GDPR requirements:

- Ability for an individual to display the personal information that
  Gerrit holds about him
- Ability to "self-remove" the personal information from Gerrit

NOTE: This plugin itself is not giving any GDPR certification or
      compliance. You would need to read carefully the EU law and
      apply to your Company context and organisation.

src/main/java/com/googlesource/gerrit/plugins/account/GerritAccountRemover.java
@@ -1,0 +1,133 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account;
+
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.api.access.PluginPermission;
+import com.google.gerrit.extensions.api.accounts.AccountApi;
+import com.google.gerrit.extensions.api.accounts.Accounts;
+import com.google.gerrit.extensions.common.EmailInfo;
+import com.google.gerrit.extensions.common.SshKeyInfo;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.google.gerrit.server.account.PutName;
+import com.google.gerrit.server.account.externalids.ExternalId;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.googlesource.gerrit.plugins.account.permissions.DeleteAccountCapability;
+import com.googlesource.gerrit.plugins.account.permissions.DeleteOwnAccountCapability;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GerritAccountRemover implements AccountRemover {
+  private static final Logger log = LoggerFactory.getLogger(GerritAccountRemover.class);
+  private final Accounts accounts;
+  private final PutName putName;
+  private final AccountResourceFactory accountFactory;
+  private final PermissionBackend permissionBackend;
+  private final Provider<CurrentUser> userProvider;
+  private final String pluginName;
+
+  @Inject
+  public GerritAccountRemover(
+      GerritApi api,
+      PutName putName,
+      AccountResourceFactory accountFactory,
+      PermissionBackend permissionBackend,
+      Provider<CurrentUser> userProvider,
+      @PluginName String pluginName) {
+    this.accounts = api.accounts();
+    this.putName = putName;
+    this.accountFactory = accountFactory;
+    this.permissionBackend = permissionBackend;
+    this.userProvider = userProvider;
+    this.pluginName = pluginName;
+  }
+
+  @Override
+  public void removeAccount(int accountId) throws Exception {
+    AccountApi account = isMyAccount(accountId) ? accounts.self() : accounts.id(accountId);
+    removeAccount(account, accountId);
+  }
+
+  private boolean isMyAccount(int accountId) {
+    return userProvider.get().getAccountId().get() == accountId;
+  }
+
+  private AccountResource getAccountResource(int accountId) {
+    return isMyAccount(accountId)
+        ? new AccountResource(userProvider.get().asIdentifiedUser())
+        : accountFactory.create(accountId);
+  }
+
+  private void removeAccount(AccountApi account, int accountId) throws Exception {
+    removeAccountEmails(account);
+    removeAccountSshKeys(account);
+    removeExternalIds(account);
+    removeFullName(getAccountResource(accountId));
+    if (account.getActive()) {
+      account.setActive(false);
+    }
+  }
+
+  @Override
+  public boolean canDelete(int accountId) {
+    PermissionBackend.WithUser userPermission = permissionBackend.user(userProvider);
+    return userPermission.testOrFalse(
+            new PluginPermission(pluginName, DeleteAccountCapability.DELETE_ACCOUNT))
+        || (userPermission.testOrFalse(
+                new PluginPermission(pluginName, DeleteOwnAccountCapability.DELETE_OWN_ACCOUNT))
+            && isMyAccount(accountId));
+  }
+
+  private void removeFullName(AccountResource userRsc) throws Exception {
+    putName.apply(userRsc, new PutName.Input());
+  }
+
+  private void removeExternalIds(AccountApi account) throws RestApiException {
+    List<String> externalIds =
+        account
+            .getExternalIds()
+            .stream()
+            .map(eid -> eid.identity)
+            .filter(eid -> !eid.startsWith(ExternalId.SCHEME_USERNAME))
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+    if (externalIds.size() > 0) {
+      account.deleteExternalIds(externalIds);
+    }
+  }
+
+  private void removeAccountSshKeys(AccountApi account) throws RestApiException {
+    List<SshKeyInfo> accountKeys = account.listSshKeys();
+    for (SshKeyInfo sshKeyInfo : accountKeys) {
+      if (sshKeyInfo != null && sshKeyInfo.valid) {
+        account.deleteSshKey(sshKeyInfo.seq);
+      }
+    }
+  }
+
+  private void removeAccountEmails(AccountApi account) throws RestApiException {
+    for (EmailInfo email : account.getEmails()) {
+      account.deleteEmail(email.email);
+    }
+  }
+}

src/test/java/com/googlesource/gerrit/plugins/account/test/DeleteAccountCommandTest.java
@@ -1,0 +1,72 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account.test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.googlesource.gerrit.plugins.account.AccountPersonalInformation;
+import com.googlesource.gerrit.plugins.account.AccountResourceFactory;
+import com.googlesource.gerrit.plugins.account.DeleteAccount;
+import com.googlesource.gerrit.plugins.account.DeleteAccountCommand;
+import com.googlesource.gerrit.plugins.account.DeleteAccountResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import org.apache.sshd.server.Environment;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteAccountCommandTest {
+
+  private DeleteAccountCommand deleteAccountCommand;
+
+  @Mock private AccountResourceFactory accountFactoryMock;
+  @Mock private DeleteAccount deleteAccountMock;
+  @Mock private Environment envMock;
+  @Mock private AccountResource accountResourceMock;
+  @Mock private IdentifiedUser userMock;
+
+  @Before
+  public void setup() throws Exception {
+    deleteAccountCommand = new DeleteAccountCommand(accountFactoryMock, deleteAccountMock);
+    deleteAccountCommand.setPrintWriters(
+        new PrintWriter(new ByteArrayOutputStream()), new PrintWriter(new ByteArrayOutputStream()));
+    DeleteAccountResponse resp =
+        new DeleteAccountResponse(true, new AccountPersonalInformation(userMock));
+    when(deleteAccountMock.apply(same(accountResourceMock), any(DeleteAccount.Input.class)))
+        .thenReturn(resp);
+  }
+
+  @Test
+  public void givenValidAccount_whenStart_shouldInvokeDeleteAccount() throws Exception {
+    int expectedAccountId = 1;
+    DeleteAccount.Input expectedInput = new DeleteAccount.Input();
+    expectedInput.accountName = "First Last";
+    when(accountFactoryMock.create(expectedAccountId)).thenReturn(accountResourceMock);
+
+    deleteAccountCommand.testRun(expectedAccountId, expectedInput.accountName);
+
+    verify(deleteAccountMock).apply(same(accountResourceMock), eq(expectedInput));
+  }
+}

src/test/java/com/googlesource/gerrit/plugins/account/test/DeleteAccountTest.java
@@ -1,0 +1,91 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.account.test;
+
+import static org.mockito.Mockito.*;
+
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.googlesource.gerrit.plugins.account.AccountRemover;
+import com.googlesource.gerrit.plugins.account.DeleteAccount;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteAccountTest {
+
+  @Mock private AccountRemover accountRemoverMock;
+
+  @Mock private AccountResource accountResourceMock;
+
+  @Mock private IdentifiedUser userMock;
+
+  @Before
+  public void setup() {
+    when(accountResourceMock.getUser()).thenReturn(userMock);
+  }
+
+  @Test
+  public void givenAccountWithPermissions_whenRunningDeleteAccount_thenAccountRemoverIsInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock).removeAccount(accountId);
+  }
+
+  @Test(expected = AuthException.class)
+  public void givenAccountWithoutPermissions_whenRunningDeleteAccount_thenThrowAuthException()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(false);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  @Test
+  public void givenAccountWithNonMatching_whenRunningDeleteAccount_thenAccountRemoverIsNotInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "NonMatching Name";
+    mockUserData(accountId, "First Last");
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  private void mockUserData(int accountId, String accountName) {
+    when(userMock.getAccountId()).thenReturn(new Account.Id(accountId));
+    when(userMock.getName()).thenReturn(accountName);
+  }
+}

GerritCodeReview/plugins_account

Change-Id: I64cdaee2803b63e05285b054cecde7be6b390149

src/main/java/com/gerritforge/gerrit/plugins/account/GerritAccountRemover.java
@@ -1,0 +1,135 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account;
+
+import com.gerritforge.gerrit.plugins.account.permissions.DeleteAccountCapability;
+import com.gerritforge.gerrit.plugins.account.permissions.DeleteOwnAccountCapability;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.api.access.PluginPermission;
+import com.google.gerrit.extensions.api.accounts.AccountApi;
+import com.google.gerrit.extensions.api.accounts.Accounts;
+import com.google.gerrit.extensions.common.EmailInfo;
+import com.google.gerrit.extensions.common.SshKeyInfo;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.account.AccountResource;
+import com.google.gerrit.server.account.PutName;
+import com.google.gerrit.server.account.externalids.ExternalId;
+import com.google.gerrit.server.permissions.PermissionBackend;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GerritAccountRemover implements AccountRemover {
+  private static final Logger log = LoggerFactory.getLogger(GerritAccountRemover.class);
+  private final Accounts accounts;
+  private final PutName putName;
+  private final AccountResourceFactory accountFactory;
+  private final PermissionBackend permissionBackend;
+  private final Provider<CurrentUser> userProvider;
+  private final String pluginName;
+
+  @Inject
+  public GerritAccountRemover(
+      GerritApi api,
+      PutName putName,
+      AccountResourceFactory accountFactory,
+      PermissionBackend permissionBackend,
+      Provider<CurrentUser> userProvider,
+      @PluginName String pluginName) {
+    this.accounts = api.accounts();
+    this.putName = putName;
+    this.accountFactory = accountFactory;
+    this.permissionBackend = permissionBackend;
+    this.userProvider = userProvider;
+    this.pluginName = pluginName;
+  }
+
+  @Override
+  public void removeAccount(int accountId) throws Exception {
+    AccountApi account = isMyAccount(accountId) ? accounts.self() : accounts.id(accountId);
+    removeAccount(account, accountId);
+  }
+
+  private boolean isMyAccount(int accountId) {
+    return userProvider.get().getAccountId().get() == accountId;
+  }
+
+  private AccountResource getAccountResource(int accountId) {
+    return isMyAccount(accountId)
+        ? new AccountResource(userProvider.get().asIdentifiedUser())
+        : accountFactory.create(accountId);
+  }
+
+  private void removeAccount(AccountApi account, int accountId) throws Exception {
+    removeAccountEmails(account);
+    removeAccountSshKeys(account);
+    removeExternalIds(account);
+    removeFullName(getAccountResource(accountId));
+    if (account.getActive()) {
+      account.setActive(false);
+    }
+  }
+
+  @Override
+  public boolean canDelete(int accountId) {
+    PermissionBackend.WithUser userPermission = permissionBackend.user(userProvider);
+    return userPermission.testOrFalse(
+            new PluginPermission(pluginName, DeleteAccountCapability.DELETE_ACCOUNT))
+        || (userPermission.testOrFalse(
+                new PluginPermission(pluginName, DeleteOwnAccountCapability.DELETE_OWN_ACCOUNT))
+            && isMyAccount(accountId));
+  }
+
+  private void removeFullName(AccountResource userRsc) throws Exception {
+    putName.apply(userRsc, new PutName.Input());
+  }
+
+  private void removeExternalIds(AccountApi account) throws RestApiException {
+    List<String> externalIds =
+        account
+            .getExternalIds()
+            .stream()
+            .map(eid -> eid.identity)
+            .filter(eid -> !eid.startsWith(ExternalId.SCHEME_USERNAME))
+            .filter(eid -> !eid.startsWith(ExternalId.SCHEME_UUID))
+            .filter(eid -> !eid.startsWith(ExternalId.SCHEME_GERRIT))
+            .filter(Objects::nonNull)
+            .collect(Collectors.toList());
+    if (externalIds.size() > 0) {
+      account.deleteExternalIds(externalIds);
+    }
+  }
+
+  private void removeAccountSshKeys(AccountApi account) throws RestApiException {
+    List<SshKeyInfo> accountKeys = account.listSshKeys();
+    for (SshKeyInfo sshKeyInfo : accountKeys) {
+      if (sshKeyInfo != null && sshKeyInfo.valid) {
+        account.deleteSshKey(sshKeyInfo.seq);
+      }
+    }
+  }
+
+  private void removeAccountEmails(AccountApi account) throws RestApiException {
+    for (EmailInfo email : account.getEmails()) {
+      account.deleteEmail(email.email);
+    }
+  }
+}

src/test/java/com/gerritforge/gerrit/plugins/account/test/DeleteAccountCommandTest.java
@@ -1,0 +1,72 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account.test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.gerritforge.gerrit.plugins.account.AccountPersonalInformation;
+import com.gerritforge.gerrit.plugins.account.AccountResourceFactory;
+import com.gerritforge.gerrit.plugins.account.DeleteAccount;
+import com.gerritforge.gerrit.plugins.account.DeleteAccountCommand;
+import com.gerritforge.gerrit.plugins.account.DeleteAccountResponse;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import org.apache.sshd.server.Environment;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteAccountCommandTest {
+
+  private DeleteAccountCommand deleteAccountCommand;
+
+  @Mock private AccountResourceFactory accountFactoryMock;
+  @Mock private DeleteAccount deleteAccountMock;
+  @Mock private Environment envMock;
+  @Mock private AccountResource accountResourceMock;
+  @Mock private IdentifiedUser userMock;
+
+  @Before
+  public void setup() throws Exception {
+    deleteAccountCommand = new DeleteAccountCommand(accountFactoryMock, deleteAccountMock);
+    deleteAccountCommand.setPrintWriters(
+        new PrintWriter(new ByteArrayOutputStream()), new PrintWriter(new ByteArrayOutputStream()));
+    DeleteAccountResponse resp =
+        new DeleteAccountResponse(true, new AccountPersonalInformation(userMock));
+    when(deleteAccountMock.apply(same(accountResourceMock), any(DeleteAccount.Input.class)))
+        .thenReturn(resp);
+  }
+
+  @Test
+  public void givenValidAccount_whenStart_shouldInvokeDeleteAccount() throws Exception {
+    int expectedAccountId = 1;
+    DeleteAccount.Input expectedInput = new DeleteAccount.Input();
+    expectedInput.accountName = "First Last";
+    when(accountFactoryMock.create(expectedAccountId)).thenReturn(accountResourceMock);
+
+    deleteAccountCommand.testRun(expectedAccountId, expectedInput.accountName);
+
+    verify(deleteAccountMock).apply(same(accountResourceMock), eq(expectedInput));
+  }
+}

src/test/java/com/gerritforge/gerrit/plugins/account/test/DeleteAccountTest.java
@@ -1,0 +1,91 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.gerrit.plugins.account.test;
+
+import static org.mockito.Mockito.*;
+
+import com.gerritforge.gerrit.plugins.account.AccountRemover;
+import com.gerritforge.gerrit.plugins.account.DeleteAccount;
+import com.google.gerrit.extensions.restapi.AuthException;
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.account.AccountResource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DeleteAccountTest {
+
+  @Mock private AccountRemover accountRemoverMock;
+
+  @Mock private AccountResource accountResourceMock;
+
+  @Mock private IdentifiedUser userMock;
+
+  @Before
+  public void setup() {
+    when(accountResourceMock.getUser()).thenReturn(userMock);
+  }
+
+  @Test
+  public void givenAccountWithPermissions_whenRunningDeleteAccount_thenAccountRemoverIsInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock).removeAccount(accountId);
+  }
+
+  @Test(expected = AuthException.class)
+  public void givenAccountWithoutPermissions_whenRunningDeleteAccount_thenThrowAuthException()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "First Last";
+    mockUserData(accountId, input.accountName);
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(false);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  @Test
+  public void givenAccountWithNonMatching_whenRunningDeleteAccount_thenAccountRemoverIsNotInvoked()
+      throws Exception {
+    int accountId = 1;
+    DeleteAccount.Input input = new DeleteAccount.Input();
+    input.accountName = "NonMatching Name";
+    mockUserData(accountId, "First Last");
+    when(accountRemoverMock.canDelete(accountId)).thenReturn(true);
+
+    new DeleteAccount(accountRemoverMock).apply(accountResourceMock, input);
+
+    verify(accountRemoverMock, times(0)).removeAccount(accountId);
+  }
+
+  private void mockUserData(int accountId, String accountName) {
+    when(userMock.getAccountId()).thenReturn(new Account.Id(accountId));
+    when(userMock.getName()).thenReturn(accountName);
+  }
+}